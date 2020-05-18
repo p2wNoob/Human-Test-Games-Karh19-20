8f0a3e0af352f4d6f574aa5d260851a26feced7e
Rosen Tenev <rosenjtenev@gmail.com>
2020-05-18 15:11:04 +0300

    added menu and reaction test game.
    Also seperated games in functions.

--- Main.ino
+++ Main.ino
@@ -43,7 +43,7 @@ class Button {
         }
 };
 
-void led_flashing(int pin, int sec){ 
+void led_flashing(int pin, float sec){ 
     digitalWrite(pin,HIGH);
     delay(1000*sec);
     digitalWrite(pin,LOW);
@@ -58,7 +58,6 @@ int led_red = 11;
 int led_white = 10;
 int led_blue = 9;
 int gameOver = 0;
-int gameOver2 = 0;
 int score = 0;
 int on_time;
 int currentLed = 0;
@@ -75,13 +74,6 @@ void setup()
   lcd.begin(20,4);
   lcd.setBacklightPin(3,POSITIVE);
   lcd.setBacklight(HIGH);
-  lcd.setCursor(0,0);
-  lcd.print("Welcome!");
-  lcd.setCursor(0,1);
-  lcd.print("Simon Says Game");
-  lcd.setCursor(0,2);
-  lcd.print("Will Start in 5 secs");
-  delay(5000);
   lcd.clear();
   b4.begin();
   b3.begin();
@@ -95,9 +87,46 @@ void setup()
   randomSeed(analogRead(0));
 }
 
-void loop()
-{  
-  if(!gameOver){
+void printMainMenu()
+{
+  lcd.setCursor(0,0);
+  lcd.print("Button 1: Simon Says");
+  lcd.setCursor(0,1);
+  lcd.print("Button 2: Guitar Hero");
+  lcd.setCursor(0,2);
+  lcd.print("Button 3: Reaction Test");
+  lcd.setCursor(0,3);
+  lcd.print("Button 4: Quit");
+}
+
+void GameStartSequence() 
+{
+  for(int i = 3; i > 0; i--)
+  {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Game starts in:");
+    lcd.print(i);
+    delay(1000);
+  }
+}
+
+void printCurrentScore(int score)
+{
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Current Score:");
+  lcd.print(score);
+}
+
+
+void SimonSays()
+{
+  gameOver = 0;
+  colorsCount = 0;
+  GameStartSequence();
+  while(!gameOver){
+    printCurrentScore(colorsCount);
     //adding a new color to the sequence
     Colors[colorsCount++] = random(9,13);
     //going through all the colors
@@ -127,34 +156,50 @@ void loop()
       }
       if(Colors[i] != playerInput && playerInput != 0){
         lcd.clear();
-        lcd.print("Game 1 Over!");
+        lcd.print("Game Over!");
         lcd.setCursor(0,1);
-        lcd.print("Score: ");
-        lcd.print(colorsCount);
-        Serial.println("\nGame 1 over!");
-            Serial.print("score: ");
-            colorsCount--;
-            Serial.print(colorsCount);
-            gameOver = 1; 
-          break;
+        lcd.print("Final score: ");
+        lcd.print(colorsCount-1);
+        gameOver = 1; 
+        break;
       }
       else if(playerInput != 0){
         i++;  
       }
     }
   }
-  else if(!gameOver2){
+  lcd.setCursor(0,2);
+  lcd.print("Press any button");
+  lcd.setCursor(0,3);
+  lcd.print("to continue");
+  delay(1000); //wait so user doenst accindetaly skip score
+  while(!b1.isReleased() && !b2.isReleased() && !b3.isReleased() && !b4.isReleased())
+  {
+    //wait until button press
+  }
+  return;
+}
+
+void GuitarHero()
+{
+ gameOver = 0;
+ score = 0;
+ GameStartSequence(); 
+  
+ while(!gameOver){
+    lcd.clear();
+    printCurrentScore(score);  
     if(currentLed == 0)
     prevLed = currentLed;
     currentLed = random(9, 13);
     if(prevLed == currentLed){
       delay(300); 
     }
-    led_flashing(currentLed, 1);
+    led_flashing(currentLed, 0.3);
     unsigned long start_time = millis();
     on_time = 0;
     unsigned long elapsed_time = 0;
-    while(elapsed_time < 3000){
+    while(elapsed_time < 300){
         unsigned long current_time = millis();
         elapsed_time = current_time - start_time;
       playerInput = 0;
@@ -172,15 +217,12 @@ void loop()
       }
       if(currentLed != playerInput && playerInput != 0){
         lcd.clear();
-        lcd.print("Game 2 Over");
+        lcd.print("Game Over!");
         lcd.setCursor(0,1);
-        lcd.print("Score: ");
+        lcd.print("Final Score: ");
         lcd.print(score);  
-        Serial.println("\nGame 2 over!");
-            Serial.print("score: ");
-            Serial.print(score);
-            gameOver2 = 1; 
-          break;
+        gameOver = 1; 
+        break;
       }
       else if(playerInput != 0){
         score++;
@@ -188,13 +230,86 @@ void loop()
             break;
       }
     }
-    if(!on_time && gameOver2 != 1){
-      Serial.println("\nGame 2 over!");
-      Serial.print("score: ");
-        Serial.print(score);
-        gameOver2 = 1;
+    if(!on_time && gameOver != 1){
+        lcd.clear();
+        lcd.print("Game Over!");
+        lcd.setCursor(0,1);
+        lcd.print("Final Score: ");
+        lcd.print(score);
+        gameOver = 1;
     }
-    
-  } 
-  delay(10); 
+  }
+  lcd.setCursor(0,2);
+  lcd.print("Press any button");
+  lcd.setCursor(0,3);
+  lcd.print("to continue");
+   delay(1000); //wait so user doenst accindetaly skip score
+  while(!b1.isReleased() && !b2.isReleased() && !b3.isReleased() && !b4.isReleased())
+  {
+    //wait until button press
+  }
+  return; 
+}
+
+void ReactionTest()
+{
+  unsigned long startTime = 0;
+  unsigned long endTime = 0;
+  unsigned long duration = 0;
+  byte timerRunning = 0;
+  float wait = random(1, 500) / 100.0;
+  byte led = random(9,13); 
+  byte button = led - 6; //gets button pin
+
+  GameStartSequence();
+  delay(wait);
+  led_flashing(led, 0.5);
+  while(1)
+  {
+    if (timerRunning == 0 && digitalRead(button) == LOW){ // button pressed & timer not running already
+    startTime = millis();
+    timerRunning = 1;
+    }
+    if (timerRunning == 1 && digitalRead(button) == HIGH){ // timer running, button released
+    endTime = millis();
+    timerRunning = 0;
+    duration = endTime - startTime;
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Score: ");
+    lcd.print(duration);
+    break;
+    }
+  }
+  lcd.setCursor(0,2);
+  lcd.print("Press any button");
+  lcd.setCursor(0,3);
+  lcd.print("to continue");
+  delay(500); //wait so user doenst accindetaly skip score
+  while(!b1.isReleased() && !b2.isReleased() && !b3.isReleased() && !b4.isReleased())
+  {
+    //wait until button press
+  }
+  return; 
+}
+
+void loop()
+{
+  while(1)
+  {
+      printMainMenu();
+      if (b1.isReleased()) {
+        SimonSays();
+      }
+      else if (b2.isReleased()) {
+        GuitarHero();
+      }
+      else if (b3.isReleased()) {
+        ReactionTest();
+      }
+      else if (b4.isReleased()) {
+        lcd.clear();
+        while(1){}
+      }
+  }
 }
